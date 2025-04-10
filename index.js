e93f809eb2ccdd703cd55a4396ed2348436e25d2
Arnav Jain <arnavjainpro@gmail.com>
2025-04-09 20:35:06 -0500

    github workshop funfact

---
+++ index.js
@@ -0,0 +1,10 @@
+function anavFunFact() {
+    const participants = [
+        {
+            name: "Arnav",
+            funFact: "I love coding and solving problems!"
+        }
+    ]
+}
+
+arnavFunFact();
